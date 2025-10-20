2b9a5557f41f8d800525917669887832143a1457
UsGitHu611 <vladislavtihonov790@gmail.com>
2025-10-21 01:55:07 +0300

    added hashRouter

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,15 +1,16 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router";
 import {App} from "../App.jsx";
 import {Main} from "../pages/main/Main.jsx";
 import {DubbingSounds} from "../pages/dubbingSounds/DubbingSounds.jsx";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
     {
         path: "/",
         element: <App/>,
         children: [
             {
                 path: "/",
+                index: true,
                 element: <Main/>
             },
             {
