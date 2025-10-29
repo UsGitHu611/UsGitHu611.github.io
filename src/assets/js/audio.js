fe63b5e1da7a1c89edd55f7396ff3b55d9b40753
UsGitHu611 <vladislavtihonov790@gmail.com>
2025-10-29 18:34:17 +0300

    added timecodes for D2

--- src/assets/js/audio.js
+++ src/assets/js/audio.js
@@ -2,6 +2,7 @@ const prefix = "https://github.com/UsGitHu611/UsGitHu611.github.io/releases/down
 
 export const audio = {
     "D1": `${prefix}/thesuffering/thesuffering.mp3`,
-    "D9": `${prefix}/commenti/commenti.mp3`
+    "D2": `${prefix}/Isaac/Isaac.mp3`,
+    "D9": `${prefix}/commenti/commenti.mp3`,
 };
 
